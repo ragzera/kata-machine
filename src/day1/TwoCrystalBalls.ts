d4bb7d7bd3ca4d30ce3112dfb0b048c0b78b50cf
Rodrigo Finkler <finklerrg@gmail.com>
2023-12-11 23:09:00 -0300

    Finished implementation of the two crystal balls problem

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jump_amount = Math.floor(Math.sqrt(breaks.length));
+    let i = jump_amount;
 
+    for (i; i < breaks.length; i += jump_amount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    i -= jump_amount;
+    console.log(`New i: ${i}`);
+    for (let j = 0; j < jump_amount && i < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
