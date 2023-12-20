364cb9a3b79cc1889bd57e97eda344fb2956e632
Rodrigo Finkler <finklerrg@gmail.com>
2023-12-19 23:05:20 -0300

    BTBFS: Implemented Binary Tree Breadth-First Search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,21 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+// BTBFS -> Binary Tree Breadth-First Search
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q: (BinaryNode<number> | null)[] = [head];
+
+    while (q.length) {
+        const curr = q.shift() as BinaryNode<number>;
+
+        // search
+        if (curr?.value === needle) {
+            return true;
+        }
+        if (curr.left) {
+            q.push(curr.left);
+        }
+        if (curr.right) {
+            q.push(curr.right);
+        }
+    }
+    return false;
 }
